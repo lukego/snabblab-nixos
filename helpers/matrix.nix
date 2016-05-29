8883d4d4f0ad195a497d1f41ce43c0e2c79eb04e
Luke Gorrie <luke@snabb.co>
2016-05-29 20:13:48 +0000

    matrix.nix: Revised tests in custom way
    Enable all DPDK and all QEMU for breadth.
    
    Rename output file '$out/lukego-log.txt' which has not been used before
    so that I can fish it out of /nix/store using 'find' to create a CSV if
    needed.
    
    Rename Snabb version to simply "dev" for brevity.

--- helpers/matrix.nix
+++ helpers/matrix.nix
@@ -73,25 +73,25 @@ let
   # define software stacks
 
   snabbs = [
-    (buildSnabb "lukegomatrix")
+    (buildSnabb "dev")
     #(buildSnabb "2016.03" "0wr54m0vr49l51pqj08z7xnm2i97x7183many1ra5bzzg5c5waky")
     #(buildSnabb "2016.04" "1b5g477zy6cr5d9171xf8zrhhq6wxshg4cn78i5bki572q86kwlx")
     #(buildSnabb "2016.05" "1xd926yplqqmgl196iq9lnzg3nnswhk1vkav4zhs4i1cav99ayh8")
   ];
   dpdks = kernel: map (x: x kernel) [
     (buildDpdk "16.04" "0yrz3nnhv65v2jzz726bjswkn8ffqc1sr699qypc9m78qrdljcfn")
-    #(buildDpdk "2.2.0" "03b1pliyx5psy3mkys8j1mk6y2x818j6wmjrdvpr7v0q6vcnl83p")
-    #(buildDpdk "2.1.0" "0h1lkalvcpn8drjldw50kipnf88ndv2wvflgkkyrmya5ga325czp")
-    #(buildDpdk "2.0.0" "0gzzzgmnl1yzv9vs3bbdfgw61ckiakgqq93b9pc4v92vpsiqjdv4")
-    #(buildDpdk "1.8.0" "0f8rvvp2y823ipnxszs9lh10iyiczkrhh172h98kb6fr1f1qclwz")
-    #(buildDpdk "1.7.1" "0yd60ww5xhf0dfl2x1pqx1m2363b2b7zp89mcya86j20gi3bgvlx")
+    (buildDpdk "2.2.0" "03b1pliyx5psy3mkys8j1mk6y2x818j6wmjrdvpr7v0q6vcnl83p")
+    (buildDpdk "2.1.0" "0h1lkalvcpn8drjldw50kipnf88ndv2wvflgkkyrmya5ga325czp")
+    (buildDpdk "2.0.0" "0gzzzgmnl1yzv9vs3bbdfgw61ckiakgqq93b9pc4v92vpsiqjdv4")
+    (buildDpdk "1.8.0" "0f8rvvp2y823ipnxszs9lh10iyiczkrhh172h98kb6fr1f1qclwz")
+    (buildDpdk "1.7.1" "0yd60ww5xhf0dfl2x1pqx1m2363b2b7zp89mcya86j20gi3bgvlx")
   ];
   qemus = [
     # TODO: https://hydra.snabb.co/build/4596
-    #(buildQemu "2.3.1" "0px1vhkglxzjdxkkqln98znv832n1sn79g5inh3aw72216c047b6")
+    (buildQemu "2.3.1" "0px1vhkglxzjdxkkqln98znv832n1sn79g5inh3aw72216c047b6")
     (buildQemu "2.4.1" "0xx1wc7lj5m3r2ab7f0axlfknszvbd8rlclpqz4jk48zid6czmg3")
-    #(buildQemu "2.5.1" "0b2xa8604absdmzpcyjs7fix19y5blqmgflnwjzsp1mp7g1m51q2")
-    #(buildQemu "2.6.0" "1v1lhhd6m59hqgmiz100g779rjq70pik5v4b3g936ci73djlmb69")
+    (buildQemu "2.5.1" "0b2xa8604absdmzpcyjs7fix19y5blqmgflnwjzsp1mp7g1m51q2")
+    (buildQemu "2.6.0" "1v1lhhd6m59hqgmiz100g779rjq70pik5v4b3g936ci73djlmb69")
   ];
   kernels = [
     linuxPackages_4_1
@@ -118,7 +118,7 @@ let
       hardware = "murren";
       inherit (attrs) snabb;
       checkPhase = ''
-        /var/setuid-wrappers/sudo ${snabb}/bin/snabb snabbmark basic1 100e6 |& tee $out/log.txt
+        /var/setuid-wrappers/sudo ${snabb}/bin/snabb snabbmark basic1 100e6 |& tee $out/lukego-log.txt
       '';
     });
   mkMatrixBenchNFVIperf = { snabb, qemu, kernel, conf, mtu, ... }@attrs:
@@ -132,7 +132,7 @@ let
       checkPhase = ''
         export SNABB_IPERF_BENCH_CONF=${confFile}
         cd src
-        /var/setuid-wrappers/sudo -E program/snabbnfv/selftest.sh bench |& tee $out/log.txt
+        /var/setuid-wrappers/sudo -E program/snabbnfv/selftest.sh bench |& tee $out/lukego-log.txt
       '';
    });
   mkMatrixBenchNFVDPDK = { snabb, qemu, kernel, dpdk, pktsize, conf, ... }@attrs:
@@ -151,7 +151,7 @@ let
 
         export SNABB_PACKET_SIZES=${pktsize}
         export SNABB_DPDK_BENCH_CONF=${confFile}
-        /var/setuid-wrappers/sudo -E timeout 160 program/snabbnfv/dpdk_bench.sh |& tee $out/log.txt
+        /var/setuid-wrappers/sudo -E timeout 160 program/snabbnfv/dpdk_bench.sh |& tee $out/lukego-log.txt
       '';
     });
   mkMatrixBenchPacketblaster = { snabb, ... }@attrs:
@@ -162,7 +162,7 @@ let
       checkPhase = ''
         cd src
         /var/setuid-wrappers/sudo ${snabb}/bin/snabb packetblaster replay --duration 1 \
-          program/snabbnfv/test_fixtures/pcap/64.pcap "$SNABB_PCI_INTEL0" |& tee $out/log.txt
+          program/snabbnfv/test_fixtures/pcap/64.pcap "$SNABB_PCI_INTEL0" |& tee $out/lukego-log.txt
       '';
     });
   mkMatrixBenchPacketblasterSynth = { snabb, ... }@attrs:
@@ -173,7 +173,7 @@ let
       checkPhase = ''
         /var/setuid-wrappers/sudo ${snabb}/bin/snabb packetblaster synth \
           --src 11:11:11:11:11:11 --dst 22:22:22:22:22:22 --sizes 64 \
-          --duration 1 "$SNABB_PCI_INTEL0" |& tee $out/log.txt
+          --duration 1 "$SNABB_PCI_INTEL0" |& tee $out/lukego-log.txt
       '';
     });
 in {
